a0c593262241a34aa6b41290bc5ae5ca0e02588d
Paul Varner <fuzzyray@gmail.com>
2021-03-04 22:49:28 -0600

    Implement fetching the stock price

--- routes/api.js
+++ routes/api.js
@@ -1,51 +1,64 @@
 'use strict';
 
+const fetch = require('node-fetch');
+
 const getStockPrice = (stockSymbol) => {
-  const result = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(
-      {'stock': stockSymbol, 'price': Math.floor(Math.random() * 100) + 1}),
-      500);
+  return new Promise((resolve, reject) => {
+    const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`;
+    fetch(url).then(response => response.json()).then(data => {
+      if (data === 'Unknown symbol') {
+        reject('Unknown symbol');
+      } else {
+        resolve({'stock': data['symbol'], 'price': data['latestPrice']});
+      }
+    }).catch(error => reject(error));
   });
-  return result;
 };
 
 const getStockLikes = (stockSymbol, ipAddress = null, like = false) => {
-  const result = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => resolve(
       {'stock': stockSymbol, 'likes': Math.floor(Math.random() * 10) + 1}),
       600);
   });
-  return result;
 };
 
 module.exports = function(app) {
 
   app.route('/api/stock-prices')
     .get(function(req, res) {
-      const symbols = (typeof req.query['stock'] === 'string')
-        ? [req.query['stock']]
-        : req.query['stock'];
-      if (symbols.length > 2) {
-        res.json({'error': 'Max of two stock symbols allowed'});
+      if (req.query.hasOwnProperty('stock')) {
+        const symbols = (typeof req.query['stock'] === 'string')
+          ? [req.query['stock']]
+          : req.query['stock'];
+        if (symbols.length > 2) {
+          res.json({'error': 'Max of two stock symbols allowed'});
+        } else {
+          const liked = req.query.hasOwnProperty('like');
+          Promise.all(symbols.map(symbol => {
+            return Promise.all(
+              [
+                getStockPrice(symbol.toUpperCase()),
+                getStockLikes(symbol.toUpperCase(), req.ip, liked)]);
+          })).then((results) => {
+            const stockData = results.map(d => {
+              return Object.assign(...d);
+            });
+            if (stockData.length === 1) {
+              return res.json({'stockData': stockData[0]});
+            } else {
+              stockData[0]['rel_likes'] = stockData[0]['likes'] -
+                stockData[1]['likes'];
+              stockData[1]['rel_likes'] = stockData[1]['likes'] -
+                stockData[0]['likes'];
+              delete stockData[0]['likes'];
+              delete stockData[1]['likes'];
+              return res.json({'stockData': stockData});
+            }
+          }).catch(error => res.json({'error': error}));
+        }
       } else {
-        Promise.all(symbols.map(symbol => {
-          return Promise.all([getStockPrice(symbol), getStockLikes(symbol)]);
-        })).then((results) => {
-          const stockData = results.map(d => {
-            return Object.assign(...d);
-          });
-          if (stockData.length === 1) {
-            return res.json({'stockData': stockData[0]});
-          } else {
-            stockData[0]['rel_likes'] = stockData[0]['likes'] -
-              stockData[1]['likes'];
-            stockData[1]['rel_likes'] = stockData[1]['likes'] -
-              stockData[0]['likes'];
-            delete stockData[0]['likes'];
-            delete stockData[1]['likes'];
-            return res.json({'stockData': stockData});
-          }
-        });
+        res.json({'error': 'No stock symbol provided'});
       }
     });
 };
